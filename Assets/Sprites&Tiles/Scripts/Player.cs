89cda72ec98ce1d97f8df41945a8a65e6adc2ec1
fogset <zhangth1990@hotmail.com>
2020-12-02 23:59:56 -0800

    fixed palyer

--- Assets/Sprites&Tiles/Scripts/Player.cs
+++ Assets/Sprites&Tiles/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Run()
+    {
+        float controlThrow = Input.GetAxis("Horizontal");
+        Vector2 playerVeloctiy = new Vector2(controlThrow, myRigidBody.y);
     }
 }
