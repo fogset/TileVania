c123c1474a90520c8a3b3d1aa0035a8cb472a330
fogset <zhangth1990@hotmail.com>
2020-12-03 21:25:56 -0800

    disabled multi-jump

--- Assets/Sprites&Tiles/Scripts/Player.cs
+++ Assets/Sprites&Tiles/Scripts/Player.cs
@@ -14,12 +14,14 @@ public class Player : MonoBehaviour
     //Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
 
     // Message then methods
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -42,6 +44,7 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))){ return; }
         if (Input.GetButtonDown("Jump"))
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
