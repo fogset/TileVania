af4e75f397a9597b58643f1ecf7acfc179c64856
fogset <zhangth1990@hotmail.com>
2020-12-08 21:48:30 -0800

    added Start Screen

---
+++ Assets/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject startButton;
+
+
+    private void Start()
+    {
+        player.SetActive(false);
+    }
+
+    public void StartFirstLevel()
+    {
+        player.SetActive(true);
+        //SceneManager.LoadScene(0);
+        startButton.SetActive(false);
+    }
+    
+
+}
