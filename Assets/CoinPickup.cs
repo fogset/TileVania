dfd8a3d31aa71ad80b7c7777a8eb8f15264a530b
fogset <zhangth1990@hotmail.com>
2020-12-09 23:57:40 -0800

    added coin for pick up

---
+++ Assets/CoinPickup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
