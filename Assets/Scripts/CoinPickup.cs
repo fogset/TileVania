0f865cfd8fcbdf7d080ac9f585df97e5962c22e0
fogset <zhangth1990@hotmail.com>
2020-12-10 22:55:44 -0800

    Persistent Score& lives

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip pickUpSound;
+    [SerializeField] int coinPickUpAmount;
   
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
         AudioSource.PlayClipAtPoint(pickUpSound, Camera.main.transform.position);
+        FindObjectOfType<GameSession>().AddToScore(coinPickUpAmount);
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3;
-   
+    [SerializeField] int score = 0;
+
+    [SerializeField] Text livesText;
+    [SerializeField] Text scoreText;
+
     private void Awake()
     {
         int numGameSessions = FindObjectsOfType<GameSession>().Length;
@@ -23,7 +28,14 @@ public class GameSession : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        livesText.text = playerLives.ToString();
+        scoreText.text = score.ToString();
+    }
+
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text = score.ToString();
     }
 
     public void ProcessPlayerDeath()
@@ -41,6 +53,7 @@ public class GameSession : MonoBehaviour
     private void TakeLife()
     {
         playerLives--;
+        livesText.text = playerLives.ToString();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
