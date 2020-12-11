7a1a2655b526a58fae5b690def886ffcc0810b68
fogset <zhangth1990@hotmail.com>
2020-12-10 22:17:32 -0800

    added coinPickUpSound

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-
-
+    [SerializeField] AudioClip pickUpSound;
+  
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(pickUpSound, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
